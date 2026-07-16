25bc5863846259674379c47d146cd6bdc7d09d1e
SabiahMdk <sabiahmuhd99@gmail.com>
2026-07-16 22:20:18 +0100

    Initial commit

---
+++ PYTHON/packages/__init__.py
@@ -0,0 +1,1 @@
+### init___.py is a special file used in python to define packages and initialize their names
---
+++ PYTHON/packages/test.py
@@ -0,0 +1,6 @@
+from packages.maths import  *
+from packages.subpackages.mult import multiply
+
+print (addition(4,3))
+print(subtraction(9,2))
+print(multiply(5,4))
