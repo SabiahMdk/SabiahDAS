b96e2926a9aef6d9e9367ead2e680a01ec850292
SabiahMdk <sabiahmuhd99@gmail.com>
2026-07-30 23:10:34 +0100

    widgets

--- PYTHON/streamlit/widgets.py
+++ PYTHON/streamlit/widgets.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 st.title("streamlist text input")
 name = st.text_input("enter your name")
 
@@ -11,4 +11,12 @@ if name:
     
 options = ["java script","java","c++","python"]
 choice = st.selectbox("choose your favorite programming language:",options)
-st.write(f"you selected  {choice}.")  
+st.write(f"you selected {choice}.") 
+
+data = {
+    "name":["John","Colin","Kelvin","Matte"],
+    "AGE":[23,17,29,27],
+    "CITY":["Nigeria","New york","India","Chicago"]
+}
+df = pd.DataFrame(data)
+st.write(df)
